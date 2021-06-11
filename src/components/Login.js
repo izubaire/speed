5e594f18c662ce2f5490514ce737113c95cfe9c2
izubaire <m.zobaire@gmail.com>
2021-06-11 10:45:17 +0500

    fix env is not defined issue

--- src/components/Login.js
+++ src/components/Login.js
@@ -47,7 +47,7 @@ export default function Login() {
             
             <div className="login__form lg:w-1/3 h-screen bg-white flex items-center justify-center w-full">
                 <form className="w-100 max-w-80%">
-                    <h3 className="text-3xl font-medium text-gray-500 mb-6">Welcome to Flame!</h3>
+                    <h3 className="text-3xl font-medium text-gray-500 mb-6">Welcome to Speed!</h3>
                     <p className="text-xl font-normal text-gray-700 mb-8">Please sign-in to your account and start the adventure</p>
 
                     <div className="inputBox mb-6">
--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { collection, addDoc, onSnapshot, serverTimestamp, query, orderBy, delete
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile, onAuthStateChanged, signOut, GoogleAuthProvider, signInWithPopup  } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: REACT_APP_API_KEY,
-  authDomain: REACT_APP_AUTH_DOMAIN,
-  projectId: REACT_APP_PROJECT_ID,
-  storageBucket: REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
-  appId: REACT_APP_APP_ID,
-  measurementId: REACT_APP_MEASUREMENT_ID,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 }
 
 const app = initializeApp(firebaseConfig)
